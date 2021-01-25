e3b07f645b1c8405f60601847dd8aa4b0494a186
liangxiaojun <fatalxiao@163.com>
2021-01-25 16:44:04 +0800

    update NavTitle

--- src/containers/app/nav/title/NavTitle.js
+++ src/containers/app/nav/title/NavTitle.js
@@ -24,18 +24,24 @@ const NavTitle = ({state}) => {
          * 获取当前 location 的 pathname
          * @type {string}
          */
-        pathName = useMemo(() => location.pathname, [location]),
+        pathName = useMemo(() =>
+                location.pathname,
+            [location]
+        ),
 
         /**
          * 根据 pathname 去 sitemap 里计算出 path
          * @type {*[]}
          */
-        navPath = useMemo(() => getPath(pathName, state), [pathName, state]);
+        navPath = useMemo(() =>
+                getPath(pathName, state),
+            [pathName, state]
+        );
 
     return (
         <div className="nav-title">
             {
-                navPath && navPath.length > 0 ?
+                navPath?.length > 0 ?
                     <div className="nav-title-crumbs-wrapper">
 
                         <i className="icon-location-pin nav-title-crumbs-icon"></i>
@@ -43,17 +49,16 @@ const NavTitle = ({state}) => {
                         <Crumbs className="nav-title-crumbs"
                                 data={navPath}
                                 separator="/"
-                                itemRenderer={(item, index) => (
-                                    index === navPath.length - 1 ?
-                                        <div className="nav-title-crumb">
-                                            {item.name}
-                                        </div>
-                                        :
-                                        <NavLink className="nav-title-crumb nav-title-link"
-                                                 to={item.route}>
-                                            {item.name}
-                                        </NavLink>
-                                )}/>
+                                itemRenderer={(item, index) => index === navPath.length - 1 ?
+                                    <div className="nav-title-crumb">
+                                        {item.name}
+                                    </div>
+                                    :
+                                    <NavLink className="nav-title-crumb nav-title-link"
+                                             to={item.route}>
+                                        {item.name}
+                                    </NavLink>
+                                }/>
 
                     </div>
                     :
