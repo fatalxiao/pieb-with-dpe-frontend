690f22fb889b57f7375e188330ce202aa4ba53cf
liangxiaojun <fatalxiao@163.com>
2021-01-26 15:42:15 +0800

    update AnalgesiaData

--- src/containers/app/modules/editPatient/analgesia/AnalgesiaData.js
+++ src/containers/app/modules/editPatient/analgesia/AnalgesiaData.js
@@ -1,4 +1,8 @@
-import React, {Component} from 'react';
+/**
+ * @file AnalgesiaData.js
+ */
+
+import React, {useMemo, useCallback, useEffect} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
@@ -6,94 +10,96 @@ import {bindActionCreators} from 'redux';
 import * as actions from 'reduxes/actions';
 import * as actionTypes from 'reduxes/actionTypes';
 
+// Components
 import AnchorButton from 'alcedo-ui/AnchorButton';
 import ModuleLoading from 'components/ModuleLoading';
 import StepAction from 'components/StepAction';
 import AnalgesiaTable from './AnalgesiaTable';
 
+// Styles
 import 'scss/containers/app/modules/editPatient/analgesiaData/AnalgesiaData.scss';
 
-class AnalgesiaData extends Component {
+const AnalgesiaData = ({
+    match, getActionType,
+    routerPush, getPatientInfo, getAnalgesiaData,
+    appendTimePoint, updatePatientStep, createOrUpdateAnalgesiaData
+}) => {
 
-    constructor(props) {
+    const
 
-        super(props);
+        /**
+         * 从路由 params 中取出的 patient ID
+         */
+        patientId = useMemo(() => match.params.patientId, [match]),
 
-        this.patientId = null;
+        /**
+         * 加载数据
+         * @type {Function}
+         */
+        loadData = useCallback(() => {
 
-        this.loadData = ::this.loadData;
-        this.prevStep = ::this.prevStep;
-        this.save = ::this.save;
+            if (!patientId) {
+                routerPush?.('/app/patient-list');
+            }
 
-    }
+            getPatientInfo?.(patientId);
+            getAnalgesiaData?.(patientId);
 
-    loadData(props = this.props) {
+        }, [
+            patientId,
+            routerPush, getPatientInfo, getAnalgesiaData
+        ]),
 
-        const {match, getPatientInfo, getAnalgesiaData} = props;
+        /**
+         * 返回上一步
+         * @type {function(): *}
+         */
+        prevStep = useCallback(() =>
+                routerPush?.(`/app/patient/info/${patientId}`),
+            [patientId, routerPush]
+        ),
 
-        if (match && match.params && match.params.patientId) {
+        /**
+         * 提交到后端
+         * @type {function(): *}
+         */
+        save = useCallback(() => createOrUpdateAnalgesiaData?.(patientId, () =>
+            routerPush(`/app/patient/observal/${patientId}`)
+        ), [patientId, routerPush, createOrUpdateAnalgesiaData]);
 
-            this.patientId = match.params.patientId;
-
-            getPatientInfo(this.patientId);
-            getAnalgesiaData(this.patientId);
-
-        } else {
-            routerPush('/app/patient-list');
-        }
-
-    }
-
-    prevStep() {
-        const {routerPush} = this.props;
-        routerPush(`/app/patient/info/${this.patientId}`);
-    }
-
-    save() {
-        const {createOrUpdateAnalgesiaData, routerPush} = this.props;
-        createOrUpdateAnalgesiaData(this.patientId, () => {
-            routerPush(`/app/patient/observal/${this.patientId}`);
-        });
-    }
-
-    componentDidMount() {
-
-        const {updatePatientStep} = this.props;
+    /**
+     * 初始化
+     */
+    useEffect(() => {
         updatePatientStep(1);
+        loadData();
+    }, []);
 
-        this.loadData();
+    return (
+        <div className="analgesia-data">
+            {
+                getActionType !== actionTypes.GET_ANALGESIA_SUCCESS ?
+                    <ModuleLoading/>
+                    :
+                    <div>
+                        <AnalgesiaTable patientId={patientId}/>
+                        <AnchorButton className="append-time-point-button"
+                                      value="Append Time Point"
+                                      onClick={appendTimePoint}>
+                            <i className="icon-chevron-thin-down down-icon"></i>
+                        </AnchorButton>
+                        <StepAction onPrev={prevStep}
+                                    onNext={save}/>
+                    </div>
+            }
+        </div>
+    );
 
-    }
-
-    render() {
-
-        const {getActionType, appendTimePoint} = this.props;
-
-        return (
-            <div className="analgesia-data">
-                {
-                    getActionType !== actionTypes.GET_ANALGESIA_SUCCESS ?
-                        <ModuleLoading/>
-                        :
-                        <div>
-                            <AnalgesiaTable patientId={this.patientId}/>
-                            <AnchorButton className="append-time-point-button"
-                                          value="Append Time Point"
-                                          onClick={appendTimePoint}>
-                                <i className="icon-chevron-thin-down down-icon"></i>
-                            </AnchorButton>
-                            <StepAction onPrev={this.prevStep}
-                                        onNext={this.save}/>
-                        </div>
-                }
-            </div>
-        );
-
-    }
-}
+};
 
 AnalgesiaData.propTypes = {
 
+    match: PropTypes.object,
     getActionType: PropTypes.string,
 
     routerPush: PropTypes.func,
