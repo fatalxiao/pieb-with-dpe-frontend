85b8178d30d66e2b71ab7a497d9bfe11e40f4932
liangxiaojun <fatalxiao@163.com>
2021-01-27 15:00:47 +0800

    update

---
+++ src/components/module/ModuleAccordion.js
@@ -0,0 +1,51 @@
+/**
+ * @file ModuleAccordion.js
+ * @author liangxiaojun(liangxiaojun@derbysoft.com)
+ */
+
+import React, {useRef, useCallback} from 'react';
+import PropTypes from 'prop-types';
+
+// Components
+import Accordion from 'alcedo-ui/Accordion';
+
+// Vendors
+import classNames from 'classnames';
+
+// Styles
+import './ModuleAccordion.scss';
+
+const ModuleAccordion = ({
+    className,
+    ...restProps
+}) => {
+
+    const
+
+        /**
+         * Accordion 的 reference
+         * @type {React.MutableRefObject<undefined>}
+         */
+        accordionRef = useRef(),
+
+        /**
+         * 重设高度
+         * @type {function(): *}
+         */
+        resetHeight = useCallback(() => accordionRef?.current?.resetHeight?.(), [accordionRef]);
+
+    return (
+        <Accordion {...restProps}
+                   ref={accordionRef}
+                   className={classNames('module-accordion', {
+                       [className]: className
+                   })}/>
+    );
+
+};
+
+ModuleAccordion.propTypes = {
+    className: PropTypes.string
+};
+
+export default ModuleAccordion;
---
+++ src/components/module/ModuleCard.js
@@ -0,0 +1,32 @@
+/**
+ * @file ModuleCard.js
+ * @author liangxiaojun(liangxiaojun@derbysoft.com)
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// Components
+import Paper from 'alcedo-ui/Paper';
+
+// Vendors
+import classNames from 'classnames';
+
+// Styles
+import './ModuleCard.scss';
+
+const ModuleCard = ({
+    className,
+    ...restProps
+}) => (
+    <Paper {...restProps}
+           className={classNames('module-card', {
+               [className]: className
+           })}/>
+);
+
+ModuleCard.propTypes = {
+    className: PropTypes.string
+};
+
+export default ModuleCard;
