f66e9ebf4ac99d99b364070ff3b39a1eb72cb1e2
liangxiaojun <fatalxiao@163.com>
2021-03-29 10:00:39 +0800

    update

--- build/dev/webpack.config.dev.js
+++ build/dev/webpack.config.dev.js
@@ -24,9 +24,7 @@ module.exports = merge(baseWebpackConfig, {
     plugins: [
 
         new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: `'${env}'`
-            }
+            'process.env.NODE_ENV': JSON.stringify(env)
         }),
 
         new webpack.HotModuleReplacementPlugin(),
--- build/prod/webpack.config.prod.js
+++ build/prod/webpack.config.prod.js
@@ -43,9 +43,7 @@ module.exports = merge(baseWebpackConfig, {
     plugins: [
 
         new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: `'${env}'`
-            }
+            'process.env.NODE_ENV': JSON.stringify(env)
         }),
 
         // new CopyPlugin([{
