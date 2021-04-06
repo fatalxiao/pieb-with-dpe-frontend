6695c6ffb8d47c01c377c96ff5b83daba2ea5db9
liangxiaojun <fatalxiao@163.com>
2021-04-06 15:10:45 +0800

    update

--- build/server/server.js
+++ build/server/server.js
@@ -1,15 +1,30 @@
-const path = require('path'),
-    express = require('express'),
-    history = require('connect-history-api-fallback'),
-    compression = require('compression'),
+/**
+ * @file server.js
+ */
 
+// Vendors
+const path = require('path');
+const express = require('express');
+const history = require('connect-history-api-fallback');
+const compression = require('compression');
+
+const
+
+    /**
+     * server port
+     * @type {string|number}
+     */
     port = process.env.port || 4000,
 
+    /**
+     * express app
+     * @type {*|Express}
+     */
     app = express();
 
 app.use(compression())
    .use(history())
-   .use(express.static(path.join(__dirname, 'dist'), {
+   .use(express.static(path.posix.join(__dirname, 'dist'), {
        setHeaders: (res, p) => {
            res.setHeader('Cache-Control', p.endsWith('index.html') ?
                'no-cache, no-store, no_store, max-age=0, must-revalidate' : 'max-age=315360000'
