dd2b41331eed7512fb8a0a21be64ac50e6aec2a9
liangxiaojun <fatalxiao@163.com>
2021-05-26 14:26:31 +0800

    update

--- src/reduxes/reducers/index.js
+++ src/reduxes/reducers/index.js
@@ -6,24 +6,12 @@
 import {combineReducers} from 'redux';
 import {connectRouter} from 'connected-react-router';
 
+// Reducers
 import loadComponent from './LoadComponentReducer';
-
-// module Reducers
-import editPatient from './app/modules/editPatient/EditPatientReducer';
-import patientBaseInfo from './app/modules/editPatient/PatientBaseInfoReducer';
-import patientInfo from './app/modules/editPatient/PatientInfoReducer';
-import analgesia from './app/modules/editPatient/AnalgesiaReducer';
-import observal from './app/modules/editPatient/ObservalReducer';
 
 export default (history, asyncReducers) => combineReducers({
 
     loadComponent,
-
-    editPatient,
-    patientBaseInfo,
-    patientInfo,
-    analgesia,
-    observal,
 
     router: connectRouter(history),
 
