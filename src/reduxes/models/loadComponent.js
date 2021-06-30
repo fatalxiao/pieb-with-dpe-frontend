fbd3d62dc885714232616006286ca330609f0fd6
liangxiaojun <fatalxiao@163.com>
2021-06-30 15:28:09 +0800

    update

--- src/reduxes/models/loadComponent.js
+++ src/reduxes/models/loadComponent.js
@@ -2,59 +2,32 @@
  * @file loadComponent.js
  */
 
-// import * as actionTypes from '../actionTypes/LoadComponentActionType';
-//
-// const initialState = {
-//     loading: false
-// };
-
 export default {
     nameSpace: 'loadComponent',
     state: {
         loading: false
     },
     reducers: {
+
+        /**
+         * 开始加载 component
+         */
         start: state => {
-            console.log('start');
             return {
                 ...state,
                 loading: true
             };
         },
+
+        /**
+         * 加载 component 完毕
+         */
         complete: state => {
-            console.log('complete');
             return {
                 ...state,
                 loading: false
             };
         }
+
     }
-    // reducer: function (state = initialState, action) {
-    //     switch (action.type) {
-    //
-    //         /**
-    //          * 开始加载 component
-    //          */
-    //         case actionTypes.LOAD_COMPONENT_START: {
-    //             return {
-    //                 ...state,
-    //                 loading: true
-    //             };
-    //         }
-    //
-    //         /**
-    //          * 加载 component 完毕
-    //          */
-    //         case actionTypes.LOAD_COMPONENT_COMPLETE: {
-    //             return {
-    //                 ...state,
-    //                 loading: false
-    //             };
-    //         }
-    //
-    //         default:
-    //             return state;
-    //
-    //     }
-    // }
 };
