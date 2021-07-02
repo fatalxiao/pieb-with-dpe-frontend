0660891f2cd59ced27993c46a9e54766d143e202
liangxiaojun <fatalxiao@163.com>
2021-07-02 16:03:44 +0800

    update

---
+++ src/reduxes/models/route.js
@@ -0,0 +1,48 @@
+/**
+ * @file notifications.js
+ */
+
+// Vendors
+import {push, replace, go, goBack, goForward} from 'connected-react-router';
+
+export default {
+    nameSpace: 'route',
+    actions: {
+
+        /**
+         * push route
+         * @param route
+         * @returns {function(*): *}
+         */
+        push: ({route}) => dispatch => dispatch(push(route)),
+
+        /**
+         * replace route
+         * @param route
+         * @returns {function(*): *}
+         */
+        replace: ({route}) => dispatch => dispatch(replace(route)),
+
+        /**
+         * go route
+         * @param route
+         * @returns {function(*): *}
+         */
+        go: ({route}) => dispatch => dispatch(go(route)),
+
+        /**
+         * go back route
+         * @param route
+         * @returns {function(*): *}
+         */
+        goBack: ({route}) => dispatch => dispatch(goBack()),
+
+        /**
+         * go forward route
+         * @param route
+         * @returns {function(*): *}
+         */
+        goForward: ({route}) => dispatch => dispatch(goForward())
+
+    }
+};
