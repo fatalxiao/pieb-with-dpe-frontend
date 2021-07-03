983220262ff197400c18639a1d9ebb6c7517ab79
liangxiaojun <fatalxiao@163.com>
2021-07-03 23:48:52 +0800

    update

--- src/modules/App/reduxes/actions/index.js
+++ src/modules/App/reduxes/actions/index.js
@@ -2,9 +2,4 @@
  * @file index.js
  */
 
-export * from './EpPlacementPointAction';
-export * from './ObservalEndPointAction';
-export * from './PatientGroupAction';
-export * from './PatientsAction';
-export * from './PatientBaseInfoAction';
 export * from './SensoryBlockAction';
